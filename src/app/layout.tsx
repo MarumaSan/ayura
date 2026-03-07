036320f1529d4fa519243fc20b207f4040d2b27b
Maruma <pongpisitpr@gmail.com>
2026-03-07 16:45:24 +0700

    feat: Add Android 192x192 and PWA 512x512 favicon sizes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,8 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: '/apple-touch-icon.png', type: 'image/png', sizes: '180x180' },
+      { url: '/android-chrome-192x192.png', type: 'image/png', sizes: '192x192' },
+      { url: '/android-chrome-512x512.png', type: 'image/png', sizes: '512x512' },
       { url: '/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
       { url: '/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
     ],
